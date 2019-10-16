c8fc4fcfcaab227512711a8b7b7c56be130fba41
Josh Houlding <jdh102040@gmail.com>
2019-10-15 22:32:57 -0700

    Fixed issue where all html pages would load simultaneously

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include_once("distance.html"); ?>
-<?php include_once("energy.html"); ?>
-<?php include_once("mass.html"); ?>
-<?php include_once("pressure.html"); ?>
-<?php include_once("speed.html"); ?>
-<?php include_once("temperature.html"); ?>
-<?php include_once("time.html"); ?>
-<?php include_once("volume.html"); ?>
+<?php //include_once("energy.html"); ?>
+<?php //include_once("mass.html"); ?>
+<?php //include_once("pressure.html"); ?>
+<?php //include_once("speed.html"); ?>
+<?php //include_once("temperature.html"); ?>
+<?php //include_once("time.html"); ?>
+<?php //include_once("volume.html"); ?>
